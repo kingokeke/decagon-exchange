78b5341cc864a1efab5a65fdb4ecf96489cf6d38
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-03-10 12:23:11 +0100

    Refactored cryptocurrency news functionality for better code clarity

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 // FUNCTION DECLARATIONS
 // ###################################
 
-function cryptoNews(article_count, letter_count, query = '', type = '') {
+function cryptoNews(number_of_articles, number_of_chars, query = '', type = '') {
   const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const API_KEY = 'f6968066194849a19cf97a517a1cd3b3';
   const SOURCE = 'crypto-coins-news';
@@ -14,30 +14,30 @@ function cryptoNews(article_count, letter_count, query = '', type = '') {
   fetch(req)
     .then(response => response.json())
     .then(data => {
-      for (let i = 0; i < article_count; i++) {
+      for (let i = 0; i < number_of_articles; i++) {
         const item = data.articles[i];
         const post = `
-          <div class="ml-3">
-            <h4><a href="${item.url}" target="_blank" class="m-b-0 font-medium p-0">${item.title}</a></h4>
-            <p class="text-muted">${item.description.slice(0, letter_count)}...<br /><a href="${
-          item.url
-        }" target="_blank">Read more</a>
-            </p>
-          </div>
-          <div class="ml-4 mr-3">
-            <div class="text-center">
-              <h5 class="text-muted m-b-0">${new Date(item.publishedAt).getDate()}</h5>
-              <span class="text-muted font-16">${MONTHS[new Date(item.publishedAt).getMonth()]}</span>
-            </div>
-          </div>
-        `;
+      <div class="ml-3">
+      <h4><a href="${item.url}" target="_blank" class="m-b-0 font-medium p-0">${item.title}</a></h4>
+      <p class="text-muted">
+        ${item.description.slice(0, number_of_chars)}... <br />
+        <a href="${item.url}" target="_blank">Read more</a>
+      </p>
+      </div>
+      <div class="ml-4 mr-3">
+      <div class="text-center">
+      <h5 class="text-muted m-b-0">${new Date(item.publishedAt).getDate()}</h5>
+      <span class="text-muted font-16">${MONTHS[new Date(item.publishedAt).getMonth()]}</span>
+      </div>
+      </div>
+      `;
         const article = document.createElement('li');
         article.classList.add('d-flex', 'no-block', 'card-body', 'border-top', 'article');
         article.innerHTML = post;
         document.querySelector('.cryptocurrency-news ul').appendChild(article);
       }
       const attribution = document.createElement('li');
-      attribution.classList.add('d-flex', 'no-block', 'card-body', 'border-top', 'article');
+      attribution.classList.add('d-flex', 'no-block', 'card-body', 'border-top', 'article', 'small');
       attribution.innerHTML = `<div class="text-center w-100"><p>Powered by <a href="https://newsapi.org" target="_blank">News API</a></p></div>`;
       document.querySelector('.cryptocurrency-news ul').appendChild(attribution);
     });
@@ -47,4 +47,4 @@ function cryptoNews(article_count, letter_count, query = '', type = '') {
 // FUNCTION CALLS
 // ###################################
 
-cryptoNews(7, 160, 'bitcoin');
+// cryptoNews(7, 160, 'bitcoin');
